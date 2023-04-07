546dcfd651c76858a6e83323cfb560a044a3c81b
user <user@DESKTOP-2PGRMF4>
2023-04-07 13:58:42 +0900

    ioex

---
+++ IOEx/src/com/chap1/A_File.java
@@ -0,0 +1,57 @@
+package com.chap1;
+
+import java.io.File;
+
+public class A_File {
+
+	public static void main(String[] args) {
+		
+		// 객체 생성
+		A_File a = new A_File();
+		// 메서드 실행 :  래퍼런스변수에 주소접근연산자(.)을 이용
+//		a.method1();
+		a.method2();
+	}
+	
+	public void method2() {
+//		File file = new File("d:/test.txt");
+		File file = new File("d:/test/test.txt");
+		File dir = new File("d:/test");
+		try {
+			// 디렉토리 한개 만들기
+			dir.mkdir();
+			
+			// 디렉토리 여러개 만들기	
+			// dir.mkdirs();    
+			
+			// 파일생성
+			file.createNewFile();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	public void method1() {
+		// 경로를 지정하지 않으면 현재 프로젝트폴더에 파일을 생성합니다.
+		// 메모리 상에만 존재
+		File file = new File("test.txt");
+		
+		
+		System.out.println("파일명 : "+ file.getName());
+		System.out.println("절대 경로 : "+ file.getAbsolutePath());
+		System.out.println("상대 경로 : "+ file.getPath());
+		System.out.println("파일 용량 : "+ file.length());
+		System.out.println("파일 존재 여부 : "+ file.exists());
+		
+		
+		// 실제 파일 생성
+		try {
+			file.createNewFile();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
